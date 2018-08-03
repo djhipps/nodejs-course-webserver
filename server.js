558bb699ce19f58718a38295d3b57da091638734
djhipps <d.j.hippisley@qmul.ac.uk>
2018-08-03 09:49:42 +0100

    added projects page

--- server.js
+++ server.js
@@ -44,6 +44,14 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Welcome to the projects page.'
+  });
+});
+
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
